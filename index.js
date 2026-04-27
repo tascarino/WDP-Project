905741902fa74e626736a5bcd0c92d52d0e0b66c
tascarino <tascarino16@gmail.com>
2026-04-27 19:59:44 -0400

    Fixed name in index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
 });
 
-app.use("/players", playerRoutes)
+app.use("/player", playerRoutes)
 
 // instead of having a domain name like, www.bestrecipes.com, 
 // we are using localhost:3500
